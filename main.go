79f13f8a05dd085d71c9a375281126d78f25ae3e
dezkoat <dezkoat13@gmail.com>
2022-02-11 01:36:00 +0700

    Connect grpc to xcntn

--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
 
-func main() {
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/dezkoat/xcntn/proto"
+)
+
+var (
+	serverAddr = flag.String("addr", "localhost:50001", "Server address")
+)
+
+func grpcInit() {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("Fail to dial: %v", err)
+	} else {
+		log.Printf("Success dialing %v", *serverAddr)
+	}
+	defer conn.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client := pb.NewContentClient(conn)
+	post, err := client.GetPost(ctx, &pb.GetPostRequest{Id: "1234"})
+	if err != nil {
+		log.Fatalf("Error calling GetPost: %v", err)
+	} else {
+		log.Printf("%v: %v", post.Title, post.Text)
+	}
+
+}
+
+func restInit() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -10,4 +49,9 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func main() {
+	grpcInit()
+	restInit()
 }
